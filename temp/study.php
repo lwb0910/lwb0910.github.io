b0429c8a69f3d00cd8e83224f0230d3048a316a5
lwb0910 <92636503+lwb0910@users.noreply.github.com>
2024-11-09 10:32:26 +0800

    Create study.php

---
+++ temp/study.php
@@ -0,0 +1,16 @@
+全国青少年信息学奥林匹克竞赛：https://www.noi.cn/
+
+C++学习网站：
+www.hterobot.com
+用户名：13552091190
+密码：123456
+
+做题网站： 
+www.luogu.com.cn，需要注册，不用花钱
+
+GESP官方网址：
+https://gesp.ccf.org.cn/
+
+NOI报名系统网址：https://cspsjtest.noi.cn/
+
+MinGW位置：C:\Program Files (x86)\Dev-Cpp\MinGW64\bin
